cb2aba5f7e851fdeb65298cd79017725598aff56
Arthur Gabriel <arthurmreboucas53@gmail.com>
2026-05-13 14:28:49 -0300

    More something. If dont't like this messages, sit and write your own commit message.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     uri = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgres:voKkxTFBAwPnpBnzzqLhxNegEwDYHByG@yamabiko.proxy.rlwy.net:43538/railway", 1)
+        uri = uri.replace("postgres://", "postgres:", 1)
 
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
