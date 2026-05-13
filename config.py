f199f784fda5f8094b636ed45869467b6b595d8d
Arthur Gabriel <arthurmreboucas53@gmail.com>
2026-05-13 17:34:23 -0300

    _

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     uri = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://postgres:",1)
+        uri = uri.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
