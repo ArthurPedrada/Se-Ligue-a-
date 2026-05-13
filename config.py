5fe031acfddcb7175a479c58d4442b6b0a3f4248
Arthur Gabriel <arthurmreboucas53@gmail.com>
2026-05-13 16:50:30 -0300

    I crash the config.py 😅

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     uri = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgres:voKkxTFBAwPnpBnzzqLhxNegEwDYHByG@yamabiko.proxy.rlwy.net:43538/railway", 1)
+        uri = uri.replace("postgres://", "postgresql://postgres:jKUlMHowpUVQhwjoWmMIcSRdwqYvMWdh@switchback.proxy.rlwy.net:21944/railway")
 
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
